9b39ba1f79a3e364dc943fc15e5810b2c97755dc
Chai Leng Tan Lim <53741855+VVentego@users.noreply.github.com>
2025-12-29 20:18:05 +0100

    Added functionality to StatsDisplay
    Stats now automatically initialise to show the stats available.

--- Assets/Scripts/PlayerStatManager.cs
+++ Assets/Scripts/PlayerStatManager.cs
@@ -1,16 +1,96 @@
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class PlayerStatManager : MonoBehaviour
 {
+    [SerializeField, Range(0, 2)]
+    private int _statNr = 0;
+
+    private StatsDisplay _stats;
+
+    public int defaultHP = 100;
+    public int maxHP;
+
+    public int defaultMP = 100;
+    public int maxMP = 100;
+
+    public BasicStats stats { get; private set; } = new();
+    public FourStats fourStats { get; private set; }
+    public SevenStats sevenStats { get; private set; }
+    public NineStats nineStats { get; private set; }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _stats = FindAnyObjectByType<StatsDisplay>();
+
+        maxHP = defaultHP;
+
+        switch (_statNr)
+        {
+            case 0:
+                fourStats = new()
+                {
+                    Strength = 0,
+                    Agility = 0,
+                    Intelligence = 0,
+                    Mind = 0
+                };
+                _stats.InitialiseFour(fourStats);
+                break;
+                case 1:
+                sevenStats = new()
+                {
+                    Attack = 0,
+                    Defense = 0,
+                    SpecialAttack = 0,
+                    SpecialDefense = 0,
+                    Speed = 0,
+                    Accuracy = 0,
+                    Evasion = 0
+                };
+                _stats.InitaliseSeven(sevenStats);
+                break;
+                case 2:
+                    nineStats = new()
+                    {
+                        Vitality = 0,
+                        Endurance = 0,
+                        Vigor = 0,
+                        Attunement = 0,
+                        Strength = 0,
+                        Dexterity = 0,
+                        Adaptabilty = 0,
+                        Intelligence = 0,
+                        Faith = 0
+                    };
+                _stats.InitialiseNine(nineStats);
+                break;
+            default:
+                fourStats = new()
+                {
+                    Strength = 0,
+                    Agility = 0,
+                    Intelligence = 0,
+                    Mind = 0
+                };
+                _stats.InitialiseFour(fourStats);
+                break;
+        }
+    }
+    
+    public FourStats GetFourStats()
+    {
+        return fourStats;
+    }
+    public SevenStats GetSevenStats() 
+    {
+        return sevenStats; 
     }
 
-    // Update is called once per frame
-    void Update()
+    public NineStats GetNineStats() 
     {
-        
+        return nineStats; 
     }
+
 }
--- Assets/Scripts/StatsDisplay.cs
+++ Assets/Scripts/StatsDisplay.cs
@@ -1,16 +1,62 @@
+using System;
+using UnityEditor.VisionOS;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StatsDisplay : MonoBehaviour
 {
+    Text[] textObjects;
+    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        textObjects = GetComponentsInChildren<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void InitialiseFour(FourStats stats)
+    {
+        for (int i = 4; i < textObjects.Length; i++)
+        {
+            textObjects[i].gameObject.SetActive(false);
+        }
+        textObjects[0].text = "Strength: " + stats.Strength.ToString();
+        textObjects[1].text = "Agility: " + stats.Agility.ToString();
+        textObjects[2].text = "Intelligence: " + stats.Intelligence.ToString();
+        textObjects[3].text = "Mind: " + stats.Mind.ToString();
+    }
+
+    public void InitaliseSeven(SevenStats stats)
+    {
+        for (int i = 7; i < textObjects.Length; i++)
+        {
+            textObjects[i].gameObject.SetActive(false);
+        }
+        textObjects[0].text = "Attack: " + stats.Attack.ToString();
+        textObjects[1].text = "Defense: " + stats.Defense.ToString();
+        textObjects[2].text = "Special Attack: " + stats.SpecialAttack.ToString();
+        textObjects[3].text = "Special Defense: " + stats.SpecialDefense.ToString();
+        textObjects[4].text = "Speed: " + stats.Speed.ToString();
+        textObjects[5].text = "Accuracy: " + stats.Accuracy.ToString();
+        textObjects[6].text = "Evasion: " + stats.Evasion.ToString();
+    }
+
+    public void InitialiseNine(NineStats stats)
+    {
+        textObjects[0].text = "Vitality: " + stats.Vitality.ToString();
+        textObjects[1].text = "Endurance: " + stats.Endurance.ToString();
+        textObjects[2].text = "Vigor: " + stats.Vigor.ToString();
+        textObjects[3].text = "Attunement: " + stats.Attunement.ToString();
+        textObjects[4].text = "Strength: " + stats.Strength.ToString();
+        textObjects[5].text = "Dexterity: " + stats.Dexterity.ToString();
+        textObjects[6].text = "Adaptabilty: " + stats.Adaptabilty.ToString();
+        textObjects[7].text = "Intelligence: " + stats.Intelligence.ToString();
+        textObjects[8].text = "Faith: " + stats.Faith.ToString();
     }
 }
