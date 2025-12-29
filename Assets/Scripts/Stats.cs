ca130b7e44d0f6ee0e040602eb8de67a2d0de12b
Chai Leng Tan Lim <53741855+VVentego@users.noreply.github.com>
2025-12-29 16:18:39 +0100

    Added basic files and Stats class

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,36 @@
+public struct BasicStats
+{
+    public int Health;
+    public int Mana;
+}
+public struct FourStats
+{
+    public int Strength;
+    public int Agility;
+    public int Intelligence;
+    public int Mind;
+}
+
+public struct SevenStats
+{
+    public int Attack;
+    public int Defense;
+    public int SpecialAttack;
+    public int SpecialDefense;
+    public int Speed;
+    public int Accuracy;
+    public int Evasion;
+}
+
+public struct NineStats
+{
+    public int Vitality;
+    public int Endurance;
+    public int Vigor;
+    public int Attunement;
+    public int Strength;
+    public int Dexterity;
+    public int Adaptabilty;
+    public int Intelligence;
+    public int Faith;
+}
